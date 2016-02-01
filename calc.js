457a6454fb098b6451c4a45c44bcbb7f6a2bf110
Jaanmk <jaan@araneus.ee>
2016-02-01 12:32:48 +0200

    calculator fixes

---
+++ calc.js
@@ -0,0 +1,25 @@
+//käivitub siis kui terve dokument laetud
+window.onload = function(){
+  console.log('laetud');
+  // saan kätte input väljad
+
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  // lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+  document.getElementById('to_cm').addEventListener('click', convertToCm);
+
+  //console.log(inch);
+};
+
+
+function convertToInch(){
+  console.log('vajutas nuppu');
+  //  arvutan ümber tollideks ja lisan väärtuse tollid lahtrisse
+  inch.value = cm.value/2.54;
+
+}
+function convertToCm(){
+  cm.value = inch.value*2.54;
+}
